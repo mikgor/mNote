d0264e6ca16257ec9911efce084d8095a310566f
mikgor <mikgorzny@gmail.com>
2018-11-27 14:21:31 +0100

    Add index & login view

--- mNoteapp/views.py
+++ mNoteapp/views.py
@@ -2,6 +2,10 @@ from django.views import generic
 from django.views.generic.edit import CreateView
 from .forms import *
 from django.urls import reverse_lazy
+from django.shortcuts import render
+
+def IndexView(request):
+    return render(request, 'mNoteapp/index.html')
 
 class SignUp(CreateView):
     form_class = AppUserCreationForm
